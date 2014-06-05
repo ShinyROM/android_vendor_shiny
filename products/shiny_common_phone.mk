30228039d8a212a6905618cb8c83cf1225298c17
Christian Gaetano <cgatno@gmail.com>
2014-06-04 23:24:04 -0400

    Set things right with our proprietary files We had to roll back some of the updates to proprietary files, but it will have to do in order to keep compatibility with tuna and ART

--- products/shiny_common_phone.mk
+++ products/shiny_common_phone.mk
@@ -8,4 +8,5 @@ PRODUCT_DEVICE := generic_phone
 
 # Private system apps
 PRODUCT_COPY_FILES += \
-	$(COMMON_PHONE_PROPS_ROOT)/priv-app/GoogleDialer.apk:system/priv-app/GoogleDialer.apk
+	$(COMMON_PHONE_PROPS_ROOT)/priv-app/GoogleDialer.apk:system/priv-app/GoogleDialer.apk \
+	$(COMMON_PHONE_PROPS_ROOT)/priv-app/GoogleDialer.odex:system/priv-app/GoogleDialer.odex
